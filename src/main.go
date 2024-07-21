7e5878a6099705ed1ec62769a42be3360e4effad
Harmohit Singh Bindra <harmohitsinghbindra@Harmohits-MacBook-Pro.local>
2024-07-20 22:04:19 -0400

    added logic to open a specific page

--- src/main.go
+++ src/main.go
@@ -1,7 +1,22 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	open_page_number(201)
+
+}
 
 func open_page_number(page_number int) {
-
+	filename := "page" + strconv.Itoa(page_number) + ".txt"
+	data, err := os.ReadFile(os.Getenv("HOME") + "/Desktop/punjabi_dictionary/sample_data/" + filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
 }
