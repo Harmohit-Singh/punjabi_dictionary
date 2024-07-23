e098447cb9f1ef94247cd006ce946f4c0900849a
Harmohit Singh Bindra <harmohitsinghbindra@Harmohits-MacBook-Pro.local>
2024-07-22 21:49:17 -0400

    added function to take input

--- src/main.go
+++ src/main.go
@@ -7,16 +7,25 @@ import (
 )
 
 func main() {
-	open_page_number(201)
-
+	input := take_unicode_input()
+	fmt.Println(input)
+	page_txt := open_page_number(201)
+	fmt.Println(string(page_txt))
 }
 
-func open_page_number(page_number int) {
+func open_page_number(page_number int) string {
 	filename := "page" + strconv.Itoa(page_number) + ".txt"
 	data, err := os.ReadFile(os.Getenv("HOME") + "/Desktop/punjabi_dictionary/sample_data/" + filename)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return "data not found"
 	}
-	fmt.Println(string(data))
+	return string(data)
+}
+
+func take_unicode_input() string {
+	var input string
+	fmt.Print("Enter word to search: ")
+	fmt.Scan(&input)
+	return input
 }
